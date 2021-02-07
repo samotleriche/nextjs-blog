aea810e036e78be982666e693956aed165cb199f
Tomas Leriche <samotleriche@gmail.com>
2021-02-07 08:56:10 -0600

    added prettier

---
+++ components/feedArea.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from '../styles/modules/FeedArea.module.css';
+
+const FeedArea = () => {
+  return (
+    <>
+    <article>
+      <a href="https://medium.com/better-programming/you-need-server-side-rendering-for-your-angular-react-vue-application-c99e55199aa5">link</a>
+    </article>
+    </>
+  )
+}
+
+export default FeedArea;
--- components/leftNav.jsx
+++ components/leftNav.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
-import styles from '../styles/modules/Header.module.css';
+import styles from '../styles/modules/LeftNav.module.css';
+
+import {
+  IoLogoGithub,
+  IoLogoMedium,
+  IoLogoLinkedin,
+  IoLogoFacebook,
+  IoLogoInstagram,
+} from 'react-icons/io5';
 
 const LeftNav = () => {
   return (
@@ -9,9 +18,33 @@ const LeftNav = () => {
         <h2>Socials</h2>
         <small>Where to find me online.</small>
       </div>
-      <hr/>
+      <hr />
+      <nav>
+        <ul>
+          <a href="https://medium.com/@samotleriche">
+            <li className={styles.navListItems}>
+              <IoLogoMedium className={styles.icons} /> Medium
+            </li>
+          </a>
+          <a href="https://github.com/samotleriche">
+            <li className={styles.navListItems}>
+              <IoLogoGithub className={styles.icons} /> Github
+            </li>
+          </a>
+          <a href="https://www.linkedin.com/in/tomasleriche/">
+            <li className={styles.navListItems}>
+              <IoLogoLinkedin className={styles.icons} /> LinkedIn
+            </li>
+          </a>
+          <a href="https://www.instagram.com/tlerichetravels/">
+            <li className={styles.navListItems}>
+              <IoLogoInstagram className={styles.icons} /> Instagram
+            </li>
+          </a>
+        </ul>
+      </nav>
     </section>
-  )
-}
+  );
+};
 
 export default LeftNav;
